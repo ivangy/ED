eaa1fb88e3d642ddeb9ddbcbf2830da9d7a2f9e7
ivan <ivansanchez989898@gmail.com>
2020-11-08 19:32:52 +0100

    ejer

---
+++ Eclipse/ejercicio_1/src/ejercicio_1/ejercicio15.java
@@ -0,0 +1,25 @@
+package ejercicio_1;
+
+public class ejercicio15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Leer num
+	suma=0
+	contador=num
+		Mientras (contador>0) 
+			Si(num%2==0) Entonces
+				suma=suma+num
+				contador=contador-1
+				num=num+2
+			SiNo
+				num=num+1
+				
+			FinSi
+		FinMientras
+		 */
+	}
+
+}
--- Eclipse/ejercicio_1/src/ejercicio_1/ejercicio7.java
+++ Eclipse/ejercicio_1/src/ejercicio_1/ejercicio7.java
@@ -6,15 +6,15 @@ public class ejercicio7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int niño, niña;
+		int ni�o, ni�a;
 		Scanner leer = new Scanner(System.in);
 		
 		System.out.println("Dime cuantos niños hay");
-		niño = leer.nextInt();
+		ni�o = leer.nextInt();
 		System.out.println("Dime cuantos niñas hay");
-		niña = leer.nextInt();
-		System.out.println("Hay " + (niño*100/(niño+niña)) + 
-				"% de niños y " + (niña*100/(niño+niña)) + "% de niñas");
+		ni�a = leer.nextInt();
+		System.out.println("Hay " + (ni�o*100/(ni�o+ni�a)) + 
+				"% de niños y " + (ni�a*100/(ni�o+ni�a)) + "% de niñas");
 	}
 
 }
