d16a63cd1e1191996392e0008865b5d4464df8be
ivan <ivansanchez989898@gmail.com>
2021-02-22 09:04:20 +0100

    aa

---
+++ Eclipse/ficheros_binarios_datos/src/ficheros_binarios_datos/IODatos.java
@@ -0,0 +1,9 @@
+package ficheros_binarios_datos;
+
+import java.io.File;
+
+public class IODatos {
+
+	File f = new File("ventas.bat");
+	
+}
---
+++ Eclipse/ficheros_binarios_datos/src/ficheros_binarios_datos/Principal.java
@@ -0,0 +1,10 @@
+package ficheros_binarios_datos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
